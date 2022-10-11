725e39c9b47c15eb81d8f848dc90f3e7c8cc6dbf
Mansur Akhmatov <mansur.ahkmatov@pinpointer.dev>
2022-10-11 17:23:18 +0300

    mac and linux support

--- back/src/Presentation/Program.cs
+++ back/src/Presentation/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddCors(options =>
     options.AddDefaultPolicy(policy =>
-        policy.WithOrigins("http://localhost", "https://localhost")));
+        policy.WithOrigins("http://localhost:3000")));
 
 // Add services to the container.
 
